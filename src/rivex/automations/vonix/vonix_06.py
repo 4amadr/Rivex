14cf12e5216303209f3dd575b470291ccd80f3ea
4amadr <vitoramadorviegas@gmail.com>
2026-01-30 13:23:18 -0300

    automação do vonix

--- src/rivex/automations/vonix/vonix_06.py
+++ src/rivex/automations/vonix/vonix_06.py
@@ -1,116 +1,108 @@
-from rivex.utils.fast_selenium import FastSelenium
-import time
+import requests
+from br4 import BeautifulSoup
 
 
-def vonix_login(driver, user, password, number_vonix):
-    fs = FastSelenium(driver, timeout=20)
-    try:
-        #user
-        fs.type_text('//*[@id="username"]', user)
-        #password
-        fs.type_text('//*[@id="password"]', password)
-        # wait until user and password get tiped
-        fs.click_button('//*[@id="wrapper"]/div/form/dl/dd[3]/input')
-        print(f'Logado no VONIX {number_vonix}!')
-        return True
-    except Exception as e:
-        print(f'Erro durante o Login no vonix {number_vonix}, type error: {e}')
-        return False
-
-def choose_vonix_client(driver):
-    '''Function to choose the client and the tean in vonix'''
-    # list with all teans
-    all_teans_list = []
-    equipes_ativas = []
-    fs = FastSelenium(driver, timeout=20)
-    fs.click_button('//*[@id="select_queues"]')
-    # aqui eu pego todas as equipes na lista de equipes para filtrar apenas as equipes ativas
-    teans = driver.find_elements('xpath', '//*[@id="queues_menu"]/li')
-    for tean in teans:
-        time_nome = tean.text
-        all_teans_list.append((time_nome, tean))
-    equipes_desativadas = ['z00', 'zdisponivel', 'manual', 'disponivel', 'disponive', 'zz', 'selecionar', 'teste',
-                               'inativos', '00']
-    for equipe, elemento_equipe in all_teans_list:
-        texto = equipe.lower()
-        if not any(palavra in texto for palavra in equipes_desativadas):
-            equipes_ativas.append((equipe, elemento_equipe))
-            print('Equipes ativas atualmente: ', equipe)
-    try:
-
-        for tamanho, (equipe_nome, elemento) in enumerate(equipes_ativas): # iterate the list for get any data with clients
-            print(f'Processando... {equipe_nome}')
-            # click in performance option
-            fs.click_button('//*[@id="maintabs"]/li[4]/a')
-            # abrir o menu suspenso de clientes
-            fs.click_button('//*[@id="select_queues"]')
-
-            # aqui o clique duplo é para remover todos os clientes
-            fs.click_button('//*[@id="queues_menu"]/li[1]/a/label')
-            #fs.click_button('//*[@id="queues_menu"]/li[1]/a/label/i')
-            # selecionar apenas a equipe iterada
-            elemento_atualizado = driver.find_element('xpath', f'//*[@id="queues_menu"]/li[contains(., "{equipe_nome}")]')
-            elemento_atualizado.click()
-            time.sleep(3)
-            # clique em um campo vazio para atualizar a lista de clientes
-            fs.click_button('//*[@id="search"]')
-            # aqui vou adicionar um tratamento de erros para lidar com páginas vazias
-            # se há data, se esse campo não aparecer a automação deve ignorar o cliente e seguir para o próximo
-            campo_data = fs.xpath_data('//*[@id="maincontent"]/table/tbody/tr[10]/th[2]')
-            if not campo_data:
-                print(f'Campos em branco para o cliente {equipe_nome} !!')
-                return None
-            else:
-                print(f'cliente selecionado!, {equipe_nome}')
-                # find yersterday data
-                date = fs.xpath_data_com_tratamento('//*[@id="maincontent"]/table/tbody/tr[10]/th[3]')
-                todas_as_chamadas_brutas = fs.xpath_data_com_tratamento('//*[@id="maincontent"]/table/tbody/tr[12]/td[3]')
-                completadas_brutas = fs.xpath_data_com_tratamento('//*[@id="maincontent"]/table/tbody/tr[13]/td[3]')
-                fracasadas_brutas = fs.xpath_data_com_tratamento('//*[@id="maincontent"]/table/tbody/tr[14]/td[3]')
-                abandonadas_brutas = fs.xpath_data_com_tratamento('//*[@id="maincontent"]/table/tbody/tr[15]/td[3]')
-
-                # vou passar os dados para str para poder ser tratados posteriormente
-                all_calls = str(todas_as_chamadas_brutas)
-                finished_calls = str(completadas_brutas)
-                fail_calls = str(fracasadas_brutas)
-                abandoned_calls = str(abandonadas_brutas)
-
-        # dicionário para armazenar os valores
-        calls_data = {
-            'date': equipe_nome,
-            'all_calls': all_calls,
-            'finished_calls': finished_calls,
-            'fail_calls': fail_calls,
-            'abandoned_calls': abandoned_calls,
+class VonixSip:
+    
+    def headers_config(url_headers):
+        '''function to config headers'''
+        
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Referer': url_headers
         }
-        print(calls_data)
-        return calls_data
-    except Exception as e:
-        print(f"Erro durante a coleta de dados do Vonix {number_vonix}: {e}!!!!!!!!")
-        return False
+        return headers
+    
+    def get_page(url_page):
+        '''function to get url and create the access'''
+        session = requests.Session()
+        
+        headers = headers_config(url_page)
+        
+        get_new_page = session.get(url_page, headers=headers)
+        soup_page = BeautifulSoup(get_new_page.text, 'html.parser')
+        
+        token_page = soup_page.find('input', {'name': 'authenticity_token'})['value']
+        if token_page:
+            return token_page
+        else:
+            print('no token')
+            return False
+    
+    def login_vonix(username, password, token, headers, queue_client: dict):
+        '''login in vonix with requests'''
+    
+        url_login = 'http://contech6.vonixcc.com.br/login/signin'
+        
+        payload_login = {
+            'authenticity_token': token,
+            'return_to': '',
+            'username': username,
+            'password': password,
+            'commit': 'Entrar'
+        }
+        
+        headers = headers_config(url_login)
+        
+        session.headers.uptate(headers)
+        
+        login = session.post(url_login, data=payload_login)
+        
+        if login.status_code != 200:
+            print(f'Error {login.status_code} in vonix login')
+        else:
+            print('login completed in vonix')
+            return session
+    
+    def filter_data(client):
+        '''function to filter the data'''
+    
+        url_filter = 'http://contech6.vonixcc.com.br/login/set_show_queue'
+        
+        headers = headers_config(url_filter)
+        
+        token = get_page(url_filter)
+        
+        payload_filter = {
+            'authenticity_token': token,
+            'return_to': '/overview',
+            'queue_id[]': queue_client['tean']
+        }
+        
+        filter = session.post(url_filter, data=payload_filter, headers=headers)
+        
+        if filter.status_code != 200:
+            print(f'error: {filter.status_code} in the vonix filter')
+        else:
+            print('filter in vonix aplied')
+            return filter
+    
+    def get_calls():
+        '''function to get the calls from vonix'''
 
-def agregar_equipes(driver, number_vonix, calls_data):
-    '''Função para agregar dados do Vonix
-    caso as equipes sejam do mesmo cliente'''
-    equipes_iguais = []
-    try:
-
-        for time in calls_data:
-            equipes_iguais.append(calls_data[time]['equipe'])
-            print(equipes_iguais)
-            return equipes_iguais
-    except Exception as e:
-        print(f"Erro ao agregar dados no Vonix {number_vonix}, type error: {e}")
-        return False
-
-user = 'victor.amador'
-password = '1801'
-url = 'http://contech6.vonixcc.com.br/'
-number_vonix = 6
-
-if __name__ == '__main__':
-    driver = FastSelenium.run_driver(url)
-    vonix_login(driver, user, password, number_vonix)
-    calls_data = choose_vonix_client(driver)
-    agregar_equipes(driver, number_vonix, calls_data)
-# em desenvolvimento...
+        calls_url = 'http://contech6.vonixcc.com.br/overview'
+        
+        headers = headers_config(calls_url)
+        
+        data = session.get(calls_url, headers=headers)
+        
+        if data.status_code != 200:
+            print(f'error{data.status_code} trying to get data from vonix.')
+            return False
+        else:
+            print('Data colected from vonix')
+            return data
+        
+    def get_agents():
+        '''function to get agents'''
+        url_agents = 'http://contech6.vonixcc.com.br/agents/calls_overview'
+        
+        headers = headers_config(url_agents)
+        
+        agents = session.get(url_agents, headers=headers)
+        
+        if agents.status_code != 200:
+            print(f'Error: {agents.status_code} trying to get agent data from vonix')
+        else:
+            print('agents data colected from vonix')
+            return agents

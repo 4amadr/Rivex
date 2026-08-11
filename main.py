54080be72162bc22633838b3b0c6386584a2908d
4amadr <vitoramadorviegas@gmail.com>
2026-08-10 23:58:43 -0300

    limpeza de main.py

--- main.py
+++ main.py
@@ -1,34 +1,12 @@
-import os
-import time
-import requests
-from dotenv import load_dotenv
-from src.rivex.enviroments.discadores.Callix.callix import CallixAPICollector
-from src.rivex.enviroments.discadores.Callix.callix_token_db import CallixDB
-from src.rivex.enviroments.discadores.vonix.equipes_vonix import dict_agentes
-from src.rivex.utils.csv_utils.callix_csv.callix_converter import CallixCSVConverter
-from src.rivex.utils.infra_utils.date_config import DateConfig
-from src.rivex.enviroments.discadores.vonix.fluxo_coleta import ExecucaoVonix
-from src.rivex.enviroments.discadores.vonix.fluxo_limpeza import LimpezaVonix
-from src.rivex.enviroments.discadores.Callix.callix_req import CAllixRequisition
-from src.rivex.data_processing.Callix.cleaner_callix_req import *
-from src.rivex.utils.database_utils.database_config import DatabaseConfig
-from src.rivex.enviroments.operadoras.gsolutions.sip_client_scrap import *
-from src.rivex.data_processing.gsolutions.cleaner_sip import *
-from dotenv import load_dotenv
-from src.rivex.data_processing.pentagono.pentagono_cleaning import *
-from src.rivex.enviroments.discadores.IPBox.colect_ipbox import *
-from src.rivex.enviroments.discadores.IPBox.payloads_ipbox import *
-from src.rivex.pipeline.pipeline_operadora.pipeline_pentagono import *
-import logging
-from src.rivex.pipeline.pipeline_discador.pipeline_ipbox import *
-from src.rivex.pipeline.pipeline_discador.pipeline_callix import *
-from src.rivex.pipeline.pipeline_operadora.pipeline_agitel import *
-from src.rivex.pipeline.pipeline_discador.pipeline_vonix import *
-from src.rivex.pipeline.pipeline_operadora.pipeline_gerax import *
-from src.rivex.pipeline.pipeline_operadora.pipeline_ultracom import *
-load_dotenv()
-logger = logging.getLogger(__name__)
-    
+from src.rivex.pipeline.pipeline_discador.pipeline_callix import PipelineCallix
+from src.rivex.pipeline.pipeline_discador.pipeline_ipbox import PipelineIpbox    
+from src.rivex.pipeline.pipeline_discador.pipeline_vonix import PipelineVonix
+from src.rivex.pipeline.pipeline_operadora.pipeline_agitel import ExecAgitel
+from src.rivex.pipeline.pipeline_operadora.pipeline_gerax import ExecucaoGerax
+from src.rivex.pipeline.pipeline_operadora.pipeline_pentagono import ExecucaoPentagono
+from src.rivex.pipeline.pipeline_operadora.pipeline_ultracom import PipelineUltracom
+
+
 def main_agitel():
     execucao = ExecAgitel()
     execucao.pipeline_agitel()
